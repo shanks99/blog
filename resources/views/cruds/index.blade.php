d6bbe27156b1320afa8a8e72382fb4dd82cb3f75
shanks999 <j1004sup@gmail.com>
2023-06-08 14:11:49 +0900

    crud view container class add

--- resources/views/cruds/index.blade.php
+++ resources/views/cruds/index.blade.php
@@ -1,56 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-    <h2 class="mt-4 mb-3">Crud List</h2>
+    <div class="container">
+        <div class="row justify-content-center">
+            <h2 class="mt-4 mb-3">Crud List</h2>
 
-    <a href="{{route("cruds.create")}}">
-        <button type="button" class="btn btn-dark" style="float: right;">Create</button>
-    </a>
+            <a href="{{ route('cruds.create') }}">
+                <button type="button" class="btn btn-dark" style="float: right;">Create</button>
+            </a>
 
-    <table class="table table-striped table-hover">
-        <colgroup>
-            <col width="15%"/>
-            <col width="45%"/>
-            <col width="15%"/>
-            <col width="25%"/>
-        </colgroup>
-        <thead>
-        <tr>
-            <th scope="col">Number</th>
-            <th scope="col">Name</th>
-            <th scope="col">Created At</th>
-            <th scope="col"></th>
-        </tr>
-        </thead>
-        <tbody>
-        {{-- blade 에서는 아래 방식으로 반복문을 처리합니다. --}}
-        {{-- Crud Controller의 index에서 넘긴 $cruds(crud 데이터 리스트)를 출력해줍니다. --}}
-        @foreach ($cruds as $key => $crud)
-            <tr>
-                <th scope="row">{{$key+1 + (($cruds->currentPage()-1) * 10)}}</th>
-                <td>
-                    <a href="{{route("cruds.show", $crud->id)}}">{{$crud->name}}</a>
-                </td>
-                <td>{{$crud->created_at}}</td>
-                <td>
-                    <a href="{{route("cruds.edit", $crud)}}" class="btn btn-warning">M</a>
+            <table class="table table-striped table-hover">
+                <colgroup>
+                    <col width="15%" />
+                    <col width="45%" />
+                    <col width="15%" />
+                    <col width="25%" />
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th scope="col">Number</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Created At</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {{-- blade 에서는 아래 방식으로 반복문을 처리합니다. --}}
+                    {{-- Crud Controller의 index에서 넘긴 $cruds(crud 데이터 리스트)를 출력해줍니다. --}}
+                    @foreach ($cruds as $key => $crud)
+                        <tr>
+                            <th scope="row">{{ $key + 1 + ($cruds->currentPage() - 1) * 10 }}</th>
+                            <td>
+                                <a href="{{ route('cruds.show', $crud->id) }}">{{ $crud->name }}</a>
+                            </td>
+                            <td>{{ $crud->created_at }}</td>
+                            <td>
+                                <a href="{{ route('cruds.edit', $crud) }}" class="btn btn-warning">M</a>
 
-                    <form action="{{route("cruds.destroy", $crud->id)}}" method="post">
-                        {{-- delete method와 csrt 처리 필요 --}}
-                        @method('delete')
-                        @csrf
-                        <button type="submit" onclick="return confirm('정말로 삭제 하시겠습니까?');" class="btn btn-danger">D</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+                                <form action="{{ route('cruds.destroy', $crud->id) }}" method="post">
+                                    {{-- delete method와 csrt 처리 필요 --}}
+                                    @method('delete')
+                                    @csrf
+                                    <button type="submit" onclick="return confirm('정말로 삭제 하시겠습니까?');"
+                                        class="btn btn-danger">D</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
-    {{-- 라라벨 기본 지원 페이지네이션 --}}
-    {!! $cruds->links() !!}
+            {{-- 라라벨 기본 지원 페이지네이션 --}}
+            {!! $cruds->links() !!}
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/cruds/show.blade.php
+++ resources/views/cruds/show.blade.php
@@ -3,10 +3,12 @@
 
 {{-- 아래 html 을 @yield('content') 에 보낸다고 생각하시면 됩니다. --}}
 @section('content')
-    <h2 class="mt-4 mb-3">Crud View: {{$crud->name}}</h2>
-    <p class="pt-2 text-right">{{$crud->created_at}}</p>
+    <div class="container">
+        <h2 class="mt-4 mb-3">Crud View: {{ $crud->name }}</h2>
+        <p class="pt-2 text-right">{{ $crud->created_at }}</p>
 
-    <div class="content mt-4 rounded-3 border border-secondary">
-        <div class="p-3">{{$crud->content}}</div>
+        <div class="content mt-4 rounded-3 border border-secondary">
+            <div class="p-3">{{ $crud->content }}</div>
+        </div>
     </div>
 @endsection
