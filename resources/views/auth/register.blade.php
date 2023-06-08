7ea58bbf158e2fe43aee2ae2e390fca0ca710f88
shanks999 <j1004sup@gmail.com>
2023-06-08 13:09:45 +0900

    test2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -134,7 +134,7 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('등록') }}
+                                    {{ __('회원가입') }}
                                 </button>
                             </div>
                         </div>
