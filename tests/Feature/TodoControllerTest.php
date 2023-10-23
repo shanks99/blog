a5dea5d37455d4478717fe449decc70a94a60798
Shanks999 <j1004sup@gmail.com>
2023-10-23 12:48:39 +0900

    testing step1

---
+++ tests/Feature/TodoControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoControllerTest extends TestCase
+{
+    /**
+     * 글 작성하기 Test
+     */
+    public function 글쓰기(): void
+    {
+        $testData = [
+            'content' => 'test 글 작성'
+        ];
+
+        $user = User::factory()->create();
+
+        $this->post(
+            route("todos.store"),
+            $testData
+        )->assertRedirect(route('todos.index')); // 응답 200
+
+        $this->assertDatabaseHas('todos', $testData);
+    }
+}
